5f4ba19bb7a02980b2aed651bb1ab13bf6bb0717
fer <ferp@protonmail.ch>
2023-04-26 17:21:53 +0200

    clean up unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import results from './datasets/eth4months/results.json'
-import heatMapRaw from './datasets/eth4months/marginHeatmap.json'
-import useMapMarketData from './core/hooks/useMapMarketData'
-import CandleChart from './components/CandleChart/CandleChart'
-import HeatMap from './components/HeatMap/HeatMap'
 import RunAnalyzer from './components/RunAnalyzer/RunAnalyzer'
 
 function App() {
