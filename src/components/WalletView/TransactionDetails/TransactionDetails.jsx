c7da8fa52e71675857a27947657622eb6586c335
fer <ferp@protonmail.ch>
2023-05-22 13:01:32 +0200

    fix crash on transction details summary

--- src/components/WalletView/TransactionDetails/TransactionDetails.jsx
+++ src/components/WalletView/TransactionDetails/TransactionDetails.jsx
@@ -16,96 +16,109 @@ const TransactionDetails = ({ selectedDate, transactionsByDate }) => {
         {open ? ">>" : "<<"}
       </Handle>
       <Contents>
-        <h3>
-          <span>
-            {selectedDate && transactionsSelectedDate?.length > 0
-              ? new Date(selectedDate).toLocaleDateString()
-              : "no date selected"}
-          </span>{" "}
-        </h3>
-        {transactionsSelectedDate.map((transaction, idx) => {
-          // calculate entry amount in quote currency
-          const entryAmount =
-            transaction?.entryPrice &&
-            parseFloat(
-              (transaction.quoteAmount / transaction.price) *
-                transaction.entryPrice
-            ).toPrecision(6);
-          // calculate transaction amount in quote currency
-          const transactionQuoteAmount = parseFloat(
-            transaction.quoteAmount.toPrecision(6)
-          );
+        {selectedDate && transactionsSelectedDate?.length === 0 ? (
+          <h3>
+            <span>"no date selected"</span>
+          </h3>
+        ) : (
+          <>
+            <h3>
+              <span>
+                {selectedDate && transactionsSelectedDate?.length > 0
+                  ? new Date(selectedDate).toLocaleDateString()
+                  : "no date selected"}
+              </span>{" "}
+            </h3>
+            {transactionsSelectedDate.map((transaction, idx) => {
+              // calculate entry amount in quote currency
+              const entryAmount =
+                transaction?.entryPrice &&
+                parseFloat(
+                  (transaction.quoteAmount / transaction.price) *
+                    transaction.entryPrice
+                ).toPrecision(6);
+              // calculate transaction amount in quote currency
+              const transactionQuoteAmount = parseFloat(
+                transaction.quoteAmount.toPrecision(6)
+              );
 
-          // calculate transaction base amount in base currency
-          const baseAmount = parseFloat(
-            transaction.quoteAmount / transaction.price
-          ).toPrecision(6);
+              // calculate transaction base amount in base currency
+              const baseAmount = parseFloat(
+                transaction.quoteAmount / transaction.price
+              ).toPrecision(6);
 
-          const marginRepayAmount = parseFloat(
-            (transaction.quoteAmount * transaction.price) /
-              transaction.entryPrice
-          ).toPrecision(6);
+              const marginRepayAmount = parseFloat(
+                (transaction.quoteAmount * transaction.price) /
+                  transaction.entryPrice
+              ).toPrecision(6);
 
-          return (
-            <Transaction key={idx}>
-              <h4>Transaction {idx + 1}</h4>
-              <p>
-                Type: <span>{transaction.type}</span>
-              </p>
-              {transaction.type != "shortCover" ? (
-                <p>
-                  Transaction Quote Amount:{" "}
-                  <span>{transactionQuoteAmount}</span>
-                </p>
-              ) : (
-                <p>
-                  Collateral Amount: <span>{transactionQuoteAmount}</span>
-                </p>
-              )}
-              {(transaction.type === "buy" || transaction.type === "short") && (
-                <p>
-                  Base Amount Acquired: <span>{baseAmount}</span>
-                </p>
-              )}
-              {transaction.type === "sell" && (
-                <>
+              return (
+                <Transaction key={idx}>
+                  <h4>Transaction {idx + 1}</h4>
                   <p>
-                    Base Amount Transacted: <span>{baseAmount}</span>
+                    Type: <span>{transaction.type}</span>
                   </p>
-                  <p>
-                    Entry Amount: <span>{entryAmount}</span>
-                  </p>
-                </>
-              )}
-              {transaction.type === "shortCover" && (
-                <>
-                  <p>
-                    Margin Repay Amount: <span>{marginRepayAmount}</span>
-                  </p>
-                  <p>
-                    Profit / Loss:{" "}
-                    <span>{transactionQuoteAmount - marginRepayAmount}</span>
-                  </p>
-                </>
-              )}
+                  {transaction.type != "shortCover" ? (
+                    <p>
+                      Transaction Quote Amount:{" "}
+                      <span>{transactionQuoteAmount}</span>
+                    </p>
+                  ) : (
+                    <p>
+                      Collateral Amount: <span>{transactionQuoteAmount}</span>
+                    </p>
+                  )}
+                  {(transaction.type === "buy" ||
+                    transaction.type === "short") && (
+                    <p>
+                      Base Amount Acquired: <span>{baseAmount}</span>
+                    </p>
+                  )}
+                  {transaction.type === "sell" && (
+                    <>
+                      <p>
+                        Base Amount Transacted: <span>{baseAmount}</span>
+                      </p>
+                      <p>
+                        Entry Amount: <span>{entryAmount}</span>
+                      </p>
+                    </>
+                  )}
+                  {transaction.type === "shortCover" && (
+                    <>
+                      <p>
+                        Margin Repay Amount: <span>{marginRepayAmount}</span>
+                      </p>
+                      <p>
+                        Profit / Loss:{" "}
+                        <span>
+                          {transactionQuoteAmount - marginRepayAmount}
+                        </span>
+                      </p>
+                    </>
+                  )}
 
-              <p>
-                Transaction Price:{" "}
-                <span>
-                  {parseFloat(transaction.price.toPrecision(precision))}
-                </span>
-              </p>
-              {transaction.entryPrice && (
-                <p>
-                  Entry Price: &nbsp;
-                  <span>
-                    {parseFloat(transaction.entryPrice.toPrecision(precision))}
-                  </span>
-                </p>
-              )}
-            </Transaction>
-          );
-        })}
+                  <p>
+                    Transaction Price:{" "}
+                    <span>
+                      {parseFloat(transaction.price.toPrecision(precision))}
+                    </span>
+                  </p>
+                  {transaction.entryPrice && (
+                    <p>
+                      Entry Price: &nbsp;
+                      <span>
+                        {parseFloat(
+                          transaction.entryPrice.toPrecision(precision)
+                        )}
+                      </span>
+                    </p>
+                  )}
+                </Transaction>
+              );
+            })}
+          </>
+        )}
       </Contents>
     </DrawerWrapper>
   );
